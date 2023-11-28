8e836e7112e84ac95551f62fd9b51796086bdd3e
teampeeta <152302872+teampeeta@users.noreply.github.com>
2023-11-28 10:40:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-scene.setBackgroundColor(3)
+let projectile: Sprite = null
+scene.setBackgroundColor(8)
+effects.starField.startScreenEffect()
 let mySprite2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . f f f f f f . f f f f f f . 
@@ -18,3 +20,28 @@ let mySprite2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite2, 90, 90)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        ..............fffc..............
+        ..............fbddcc............
+        ..ccc.........ffbddbc...........
+        ..cbbc.........fcbbccf..........
+        ...cbdc.......ffccccccfffffff...
+        ...fbdc....fffcbbbbbbbbbbbbbcff.
+        ....fbdc.ffcccbbbbbbcbbbbbbbbbcf
+        ....fcdffcccccbbbcbcbbbffbbbbcbf
+        ....fcbbccccccbbbcbcbbbff1111bbf
+        ...fcbbccccccccbbbcbb11111111bf.
+        ...fbbfffcccccccbbbb11bc33cccf..
+        ..fbbf..cbdbcccccbbb111c131cf...
+        ..fff....cbdddddccbbc111c33f....
+        ..........ccbddccbbdf1111ff.....
+        ............ccfbbbdfccccc.......
+        ..............fffff.............
+        `, mySprite2, randint(-25, 700), randint(-25, 25))
+    projectile.lifespan = 3000
+    projectile.startEffect(effects.fire)
+    if (projectile.vx < 0) {
+        projectile.image.flipX()
+    }
+})
