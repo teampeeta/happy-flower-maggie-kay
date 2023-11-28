ba381be1ddb1c33790e0176901248ca90720cf66
teampeeta <152302872+teampeeta@users.noreply.github.com>
2023-11-28 10:11:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+let MySprite: Sprite = null
+scene.setBackgroundColor(3)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f . f f f f f f . 
+    . f f 3 3 3 3 f f f 3 3 3 3 f f 
+    . f 3 3 3 3 3 3 f 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+    . f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+    . f 3 3 3 3 3 b b b 1 1 1 3 3 f 
+    . f 3 3 3 3 b b b b b 3 3 3 3 f 
+    . f f 3 3 b b b b b b b 3 3 f f 
+    . . f f 3 b b b b b b b 3 f f . 
+    . . . f f b b b b b b b f f . . 
+    . . . . f f b b b b b f f . . . 
+    . . . . . f f b b b f f . . . . 
+    . . . . . . f f b f f . . . . . 
+    . . . . . . . f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(MySprite, 90, 90)
